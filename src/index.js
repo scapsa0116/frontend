28a490eba9f0dd2f4b60b5369369a12d4702cb16
github username <github email address>
2021-03-15 21:49:14 -0500

    set up redux files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import{ cerateStore, applyMiddleware} from 'redux';
+import{ createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 
 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+
 
 ReactDOM.render(
-  <Router>
-  <Route path="/" component={App}/>
-  </Router>,
+  <Provider store={store}>
+  <App/>
+  </Provider>,
    
   document.getElementById('root')
 );
